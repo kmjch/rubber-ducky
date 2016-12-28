60359a110570232c3193421a4109318c8c8bc057
Michelle <jhmichellekim@gmail.com>
2016-12-28 01:52:49 -0800

    Revert "added new draw ducky feature"
    This reverts commit 9e7d847a3920e974bdda54da75c6037bd6495c1a.

--- spec/rubber-ducky-spec.js
+++ spec/rubber-ducky-spec.js
@@ -43,10 +43,6 @@ describe('RubberDucky', () => {
 
         atom.commands.dispatch(workspaceElement, 'rubber-ducky:respond');
         expect(rubberDuckyPanel.isVisible()).toBe(false);
-
-        atom.commands.dispatch(workspaceElement, 'rubber-ducky:draw');
-        expect(rubberDuckyPanel.isVisible()).toBe(false);
-
       });
     });
 
